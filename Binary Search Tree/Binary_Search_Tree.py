b6edc0b5ea0fa7df8fd6b45fbb943d6d6a1f1563
ahmedabdelsalam <ahmedfw2178@gmail.com>
2022-11-05 14:47:29 +0200

    Binary Search Tree 1

---
+++ Binary Search Tree/Binary_Search_Tree.py
@@ -0,0 +1,31 @@
+"""
+ADS :
+    insert() 
+    delete()
+    get_tree_height()
+    traversal :
+        in order
+        pre order
+        post order
+    level traversal
+        print
+    get_size
+    get_node_depth()
+    get_node_height()
+
+"""
+class BST:
+
+    def __init__(self):
+        self.root = Node()
+        self.size = 0
+
+
+
+
+class Node:
+    def __init__(self):
+        self.val = 0
+        self.left = self.right = None
+
+   
---
+++ Binary Search Tree/run.bat
@@ -0,0 +1,2 @@
+python Binary_Search_Tree.py 
+cmd /k
