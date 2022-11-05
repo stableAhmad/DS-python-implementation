319e54ad7f715020f937118917a6b113a430a641
ahmedabdelsalam <ahmedfw2178@gmail.com>
2022-11-05 16:31:44 +0200

    BST

--- Binary Search Tree/Binary_Search_Tree.py
+++ Binary Search Tree/Binary_Search_Tree.py
@@ -1,6 +1,6 @@
 """
 ADS :
-    insert() 
+    insert()                done    
     delete()
     get_tree_height()
     traversal :
@@ -9,7 +9,7 @@ ADS :
         post order
     level traversal
         print
-    get_size
+    get_size()              done        tested
     get_node_depth()
     get_node_height()
 
@@ -17,12 +17,47 @@ ADS :
 class BST:
 
     def __init__(self):
-        self.root = Node()
+        self.root = None
         self.size = 0
 
+    def get_root(self):
+        return self.root
 
+    def insert(self , val):
+        new_node = Node()
+        new_node.val = val
+        root = self.get_root()
+        if root :
+            current = root
+            while(True):
+                if(new_node.val < current.val):
+                    if(current.left):
+                        current = current.left
+                    else:
+                        current.left = new_node
+                        break
+                else:
+                    if(current.right):
+                        current = current.right
+                    else:
+                        current.right = new_node
+                        break
 
+        else:
+            self.root = new_node
+        self.size+=1
 
+    def get_size(self):
+        return self.size
+    def in_order_traversal(self):
+        self.in_recursion(self.root)
+
+    def in_recursion(self , node):
+        if not node:
+            return
+        self.in_recursion(node.left)
+        print(node.val , end=" ")
+        self.in_recursion(node.right)
 class Node:
     def __init__(self):
         self.val = 0
